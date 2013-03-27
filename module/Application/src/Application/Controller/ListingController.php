47c4a16ea211cd830bab2e08d8919bf4f2343a53
Ralf Eggert <ralf@travello.com>
2013-03-27 18:28:38 +0100

    Projektdateien bereit gestellt

---
+++ module/Application/src/Application/Controller/ListingController.php
@@ -0,0 +1,199 @@
+<?php
+/**
+ * ZF2 Buch Kapitel 15
+ * 
+ * Das Buch "Zend Framework 2 - Von den Grundlagen bis zur fertigen Anwendung"
+ * von Ralf Eggert ist im Addison-Wesley Verlag erschienen. 
+ * ISBN 978-3-8273-2994-3
+ * 
+ * @package    Application
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @copyright  Alle Listings sind urheberrechtlich geschützt!
+ * @link       http://www.zendframeworkbuch.de/ und http://www.awl.de/2994
+ */
+
+/**
+ * namespace definition and usage
+ */
+namespace Application\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+/**
+ * Listing controller
+ * 
+ * Shows and executes the listings
+ * 
+ * @package    Application
+ */
+class ListingController extends AbstractActionController
+{
+    /**
+     * Redirects to homepage
+     */
+    public function indexAction()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.01
+     */
+    public function listing1501Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.02
+     */
+    public function listing1502Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.03
+     */
+    public function listing1503Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.04
+     */
+    public function listing1504Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.05
+     */
+    public function listing1505Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.06
+     */
+    public function listing1506Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.07
+     */
+    public function listing1507Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.08
+     */
+    public function listing1508Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.09
+     */
+    public function listing1509Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.10
+     */
+    public function listing1510Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.11
+     */
+    public function listing1511Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.12
+     */
+    public function listing1512Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.13
+     */
+    public function listing1513Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.14
+     */
+    public function listing1514Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.15
+     */
+    public function listing1515Action()
+    {
+        return new ViewModel();
+    }
+
+    /**
+     * Show listing 15.16
+     */
+    public function listing1516Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.17
+     */
+    public function listing1517Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.18
+     */
+    public function listing1518Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.19
+     */
+    public function listing1519Action()
+    {
+        return new ViewModel();
+    }
+    
+    /**
+     * Show listing 15.20
+     */
+    public function listing1520Action()
+    {
+        return new ViewModel();
+    }
+}
